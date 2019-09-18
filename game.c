fbf001441323d13ae7bc449285e49991c4224221
Ubuntu <ubuntu@ip-172-31-93-165.ec2.internal>
2019-09-18 17:32:42 +0000

    hell oworld for gameboy

---
+++ game.c
@@ -0,0 +1,15 @@
+// Copyright (c) 2019 St. Mother Teresa HS All rights reserved.
+//
+// Created by: Joey Marcotte
+// Created on: Sept 2019
+// This program is the "Hello, World!" program for the GameBoy
+
+
+#include <stdio.h>
+#include <gb/gb.h>
+
+int main() {
+    printf("Hello, World!");
+
+    return 0;
+}
